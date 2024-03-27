2a3c6ab45db03151f1703100c34b50aa7d990263
MauricioDCH <mdcorreah@eafit.edu.co>
2024-03-27 18:44:47 -0500

    OtrasPruebas

---
+++ W10/otrasPruebas/2/server_1.c
@@ -0,0 +1,72 @@
+#include <stdio.h> 
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <sys/types.h> 
+#include <unistd.h> // read(), write(), close()
+#include <arpa/inet.h>
+
+#define MAX 80 
+#define PORT 8080 
+#define SOCKETADDRESS struct sockaddr
+
+// Driver function 
+int main() 
+{ 
+	int my_socket, connfd, len; 
+	struct sockaddr_in server_address, client;
+
+	// SOCKET 
+	my_socket = socket(AF_INET, SOCK_STREAM, 0); 
+	if (my_socket < 0) { 
+		printf("socket creation failed...\n"); 
+		exit(0); 
+	} 
+    else
+        printf("Socket successfully created..\n");
+	printf("Socket successfully created..\n");
+
+    // Asignación de IP, PORT 
+    server_address.sin_family = AF_INET; 
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(PORT);
+
+    // BIND
+    if(bind(my_socket, (SOCKETADDRESS*)&server_address, sizeof(server_address))== -1){
+        printf("socket bind failed...\n");
+        exit(0);
+    }
+    else{
+        printf("Socket successfully binded..\n");
+    }
+
+    // LISTEN
+    int backlog = 5; // máxima cantidad de conexiones pendientes
+    int status = listen(my_socket, backlog);
+    if(status == -1){
+        printf("Listen failed...\n");
+        exit(0);
+    }
+    else{
+        printf("Server complete\n");
+    }
+
+    // ACCEPT
+    
+    len = sizeof(client);
+    // Accept the data packet from client and verification 
+    connfd = accept(my_socket, (SOCKETADDRESS*)&client, &len); 
+    if (connfd < 0) { 
+        perror("server accept failed...\n"); // Manejo de errores
+        exit(EXIT_FAILURE); 
+    } 
+    else
+        printf("server accept the client...\n"); 
+    // READ
+    // WRITE
+    // READ
+    // CLOSE
+    return 0;
+}
